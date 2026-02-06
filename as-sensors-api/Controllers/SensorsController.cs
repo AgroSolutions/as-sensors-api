0438c8836eb01db162676e4a99495669c72c2eeb
Fernanda Rocha Miranda <fernandamirandacursos@gmail.com>
2026-02-06 17:55:21 -0300

    Add project files.

---
+++ as-sensors-api/Controllers/SensorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AS.Sensors.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SensorsController : ApiBaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetCart()
+        {
+            return Success("Carrinho encontrado/criado com sucesso.");
+        }
+    }
+}
