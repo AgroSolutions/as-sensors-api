3c329b5508184c4ec3ae34d2b6a032ba767b0741
Fernanda Rocha Miranda <fernandamirandacursos@gmail.com>
2026-02-21 21:30:00 -0300

    ajuste

--- as-sensors-api/Program.cs
+++ as-sensors-api/Program.cs
@@ -3,15 +3,20 @@ using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson;
+using as_sensors_api.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+#region Swagger
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerConfiguration();
+#endregion
 
 #region DI
 builder.Services.AddSingleton<MongoContext>();
@@ -26,10 +31,22 @@ builder.Services.AddScoped<as_sensors_infra.Persistance.Repository.Interfaces.IS
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
+//app.UseSwagger();
+//app.UseSwaggerUI();
 
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsProduction())
+{
+    app.UseSwaggerConfiguration();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
+app.UseDeveloperExceptionPage();
+app.UseExceptionHandler("/Error");
+app.UseHsts();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
