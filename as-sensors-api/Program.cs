d34092f3702336322e28f29b0ca8cef50a850cdb
Fernanda Rocha Miranda <fernandamirandacursos@gmail.com>
2026-02-21 21:01:27 -0300

    ajuste

--- as-sensors-api/Program.cs
+++ as-sensors-api/Program.cs
@@ -26,11 +26,9 @@ builder.Services.AddScoped<as_sensors_infra.Persistance.Repository.Interfaces.IS
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
